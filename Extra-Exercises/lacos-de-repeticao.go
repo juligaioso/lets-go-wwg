dd7550dcdd2b96d4606d36a538fd6603b91acfa8
Juliana Gaioso <37074547+juligaioso@users.noreply.github.com>
2021-09-12 21:41:41 -0300

    fix: nome do arquivo

--- Extra-Exercises/lacos-de-repeticao.go
+++ Extra-Exercises/lacos-de-repeticao.go
@@ -1,52 +1,15 @@
-package mainpackage main
+package mainpackage
+package main
 
 import (
 "fmt"
 )
 
 func main() {
-	// Exercício #01
-	// Faça um programa em que 3 variáveis recebem valores diferentes e informa qual a variável com maior valor.
-	a := 9
-	b := 7
-	c := 3
-
-	if a > b {
-		if a > c {
-			fmt.Println(a)
-		} else {
-			fmt.Println(c)
-		}
-	} else {
-		if b > c {
-			fmt.Println(b)
-		} else {
-			fmt.Println(c)
-		}
+	// No Exercício #06 da seção "Exercícios", usamos for range para percorrer uma slice de string que representava uma lista de itens a comprar no mercado.
+	// Agora, resolva o mesmo exercício usando a sintaxe básica da instrução for (sintaxe apresentada aqui).t
+	var listaDeCompra = []string{"Arroz", "Feijão", "Batata", "Óleo", "Sal", "Café"}
+	for i := 0; i < len(listaDeCompra); i++ {
+		fmt.Printf("%d - %s\n", i+1, listaDeCompra[i])
 	}
-
-	// Exercício #02
-	// Faça um programa que testa se um número passado em uma variável é 0, múltiplo de 2, múltiplo de 3 ou nenhuma das opções.
-
-	d := 2
-
-	if d == 0 {
-		fmt.Printf("O número é zero")
-		return
-	}
-	if d%2 == 0 && d%3 == 0 {
-		fmt.Printf("O número é múltiplo de 2 e de 3")
-		return
-	}
-	if d%2 == 0 {
-		fmt.Printf("O número é múltiplo de 2")
-		return
-	}
-	if d%3 == 0 {
-		fmt.Printf("O número é múltiplo de 3")
-		return
-	}
-	fmt.Printf("O número não é igual a zero, múltiplo de 2 ou de 3")
-
 }
-
